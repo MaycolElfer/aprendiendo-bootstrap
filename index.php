6110d17f9faf78a19eb234ce9556961ec38ce551
james <floresrui123@gmail.com>
2016-03-10 15:24:14 -0500

    trabajo de pagina web hecho en index.html index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>deportivos</title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">DEPORTIVOS</a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">AUTOS</a></li>
+
+						<li><a href="#">MOTOCROS</a></li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Search">
+						</div>
+					           </form>
+					<ul class="nav navbar-nav navbar-right">
+
+					<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">MARCAS DE AUTOMOVILES <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								  <li><a href="#">CARROS FERRARI</a></li>
+								  <img src="public/imagenes/ferrari.jpg" alt="">
+								  <li><a href="#">PULSAR</a></li>
+								  <img src="public/imagenes/yhamaja.jpg" alt="">
+							      <li><a href="#">YAMAHA</a></li>
+							      <img src="public/imagenes/pulsar.jpg" alt="">
+	                               <li><a href="#">NISSAN</a></li>
+	                               <img src="public/imagenes/nissan.jpg" alt="">
+								   
+							</ul>
+						</li>
+								     
+							</ul>
+
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+
+					<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">PRECIOS DE AUTOMOVILES <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								  <li><a href="#">CARROS FERRARI $ 100.OOO</a></li>
+								   <img src="public/imagenes/ferrari.jpg" alt="50">
+								  <li><a href="#">PULSAR $ 15.000</a></li>
+							        <img src="public/imagenes/pulsar.jpg" alt="50">
+							       <li><a href="#">YAMAHA $ 16.OOO</a></li>
+								     <img src="public/imagenes/yhamaja.jpg" alt="50">
+								     <li><a href="#">NISSAN $ 80.000</a></li>
+								     <img src="public/imagenes/nissan.jpg" alt="30">
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+		<!-- CAROUSEL -->
+		<div id="carousel-id" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item">
+					<img data-src="public/imagenes/pulsar.jpg" alt="First slide" src="public/imagenes/pulsar.jpg" width="1000">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/ferrari.jpg" src="public/imagenes/ferrari.jpg" width="1200">
+					
+				</div>
+				<div class="item active">
+					<img data-src="public/imagenes/yhamaja.jpg" alt="Third slide" src="public/imagenes/yhamaja.jpg" width="990">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/ferrari.jpg" src="public/imagenes/ferrari.jpg" width="990">
+					
+				</div>
+			</div>
+			<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+
+	</div>
+
+
+
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+
+	<embet ></embet>
+</body>
+</html>
